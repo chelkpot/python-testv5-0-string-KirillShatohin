e1c0dd4238e88071504f1cef386aa1acca5ee345
student <student@corp.kpiot.ru>
2025-10-21 10:30:26 +0500

    sdadsdsa

--- tasks/task1.py
+++ tasks/task1.py
@@ -3,7 +3,7 @@
 def solve():
 # Ниже пишите решение задачи
     word=input()
-    print(word + " " + word + "" + word + " " + word )
+    print(word + " " + word + " " + word + " " + word)
         
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
